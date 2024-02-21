dcff679f144abd6d8cac3925b105c2defc095c28
frantecbh <franciscomenezesbh@gmail.com>
2024-02-20 22:51:31 -0300

    meddwere de autenticação

---
+++ src/middlewares/ensureAuthenticated.js
@@ -0,0 +1,28 @@
+import { verify } from 'jsonwebtoken'
+import { AppError } from '../utils/AppError.js'
+import { authConfig } from '../configs/auth.js'
+
+export function ensureAutheticated(request, response, next) {
+  const authToken = request.headers.authorization
+
+  if (!authToken) {
+    throw new AppError('JWT Token não informado', 401)
+  }
+
+  // verify Bare aldfjlasjdflasjfdlalfjsaf
+  const [, token] = authToken.split(' ')
+
+  const { secret } = authConfig.jwt
+
+  try {
+    const { sub: user_id } = verify(token, secret)
+
+    request.user = {
+      id: user_id,
+    }
+
+    return next()
+  } catch {
+    throw new AppError('JWT Token invalido', 401)
+  }
+}
